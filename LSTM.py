12729a94e73149190e469b84907de4fd83b37ff5
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2024-06-27 19:50:53 -0500

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -27,7 +27,33 @@ from tensorflow.keras.layers import Dense, Input, Dropout,BatchNormalization
 from tensorflow.keras import layers
 from tensorflow.keras import backend
 
+import pandas as pd
 
+# Read the Excel file
+df = pd.read_csv('/content/drive/MyDrive/BitcoinRunes/BILLION•DOLLAR•CAT.csv')
+
+# Display the first few rows of the DataFrame
+print(df.head())
+
+df.drop(['symbol', 'etching_time', 'turbo', 'inscription_id' ], axis=1, inplace=True)
+
+
+# Load the data from a CSV file
+
+# Assign the loaded DataFrame to a variable
+final_data = df.copy()
+
+# Convert the 'timestamp' column to datetime, handling various formats
+final_data['timestamp'] = pd.to_datetime(final_data['timestamp'], errors='coerce')
+
+# Remove milliseconds and timezone from the 'timestamp' column
+final_data['timestamp'] = final_data['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+# Set the 'timestamp' column as the index of the DataFrame
+final_data.set_index('timestamp', inplace=True)
+
+# Print the final DataFrame to verify the result
+print(final_data)
 # Ensure random initialization of the seed
 seed_value = random.randint(0, 10000)
 np.random.seed(seed_value)
